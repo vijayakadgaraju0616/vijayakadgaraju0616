375a156968bedd2a04e6c82f463d8cd79adab482
vijayakadgaraju0616 <vijaya.kadgaraju@gmail.com>
2025-06-16 21:43:38 +0530

    Typecasting lab1

---
+++ src/com/learningjava/TypeCasting/lab1typecasting.java
@@ -0,0 +1,10 @@
+package com.learningjava.TypeCasting;
+
+public class lab1typecasting {
+    public static void main(String[] args)
+    {
+        byte b=10;
+        int a=b;// widening implicit casting
+        int a1=(int)b;//same
+    System.out.println(a);}
+}
