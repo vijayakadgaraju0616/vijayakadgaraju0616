1057f9965ad176e52be60b35505cebba555207d8
vijayakadgaraju0616 <vijaya.kadgaraju@gmail.com>
2025-06-24 22:47:27 +0530

    VariableDeclaration_Initialization

---
+++ src/com/learningjava/Assignment1/VariableDeclaration_Initialization.java
@@ -0,0 +1,19 @@
+package com.learningjava.Assignment1;
+
+public class VariableDeclaration_Initialization {
+        public static void main(String[] args) {
+            // Variable declarations with initialization
+            int number = 25;
+            double piValue = 3.14;
+            boolean isAvailable = true;
+            char grade = 'A';
+
+            // Printing values
+            System.out.println("Integer: " + number);
+            System.out.println("Double: " + piValue);
+            System.out.println("Boolean: " + isAvailable);
+            System.out.println("Character: " + grade);
+        }
+    }
+
+
